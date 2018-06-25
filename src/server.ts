c0d82c25738b77bba5bbeb007e6a143ca35dc262
Daksh Miglani <hello@dak.sh>
2018-06-25 15:37:12 +0530

    setup rate limiting

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { GraphQLServer } from 'graphql-yoga';
 import { formatError } from 'apollo-errors';
 import { join } from 'path';
 import * as helmet from 'helmet';
+import * as RateLimit from 'express-rate-limit';
 import { Resolvers as resolvers } from './graphql/resolvers';
 import router from './routes';
 
@@ -17,5 +18,12 @@ export const Server = new GraphQLServer({
 	typeDefs: join(__dirname, './graphql/schema.graphql'),
 });
 
-Server.express.use('/', router);
 Server.express.use(helmet());
+Server.express.use(new RateLimit({
+	windowMs: 10 * 1000 * 60, // 10 minute window
+	max: 100, // 100 requests per windowMs
+	delayMs: 0,
+	message: 'Too many requests created from this IP, please try again after 10 minutes.',
+}));
+
+Server.express.use('/', router);
