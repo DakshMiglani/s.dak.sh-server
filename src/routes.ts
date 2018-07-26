06ae526b9a1f2d34336306a472fe7e41dcc4928e
Daksh Miglani <hello@dak.sh>
2018-07-27 00:55:44 +0530

    create-url function.

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Response, Request } from "express";
+import { isURL } from "validator";
 import { homeURL } from "./constants";
 import * as shortid from "shortid";
 import { Model } from "./models/url";
@@ -27,6 +28,26 @@ router.get("/:uuid", async (req: Request, res: Response) => {
   }
 
   return res.redirect(NOT_FOUND); // redirect to 404.
+});
+
+router.post("/create-url", async (req: Request, res: Response) => {
+  if (req.body["url"] && isURL(req.body["url"])) {
+    const URL = new Model({
+      redirectTo: req.body["url"]
+    });
+
+    try {
+      await URL.save();
+      res.json({ url: `${homeURL}/${URL.uuid}` });
+    } catch (e) {
+      res.json({
+        url: null,
+        error: "Cannot Save the URL."
+      });
+    }
+  } else {
+    res.json({ url: null, error: "URL was not provided." });
+  }
 });
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as http from "http";
 import * as helmet from "helmet";
 import * as RateLimit from "express-rate-limit";
+import * as BodyParser from "body-parser";
 import router from "./routes";
 
 const app = express();
@@ -9,6 +10,7 @@ const opts = { port: 4000 };
 const Server = http.createServer(app);
 
 app.use(helmet());
+app.use(BodyParser.json());
 app.use(
   new RateLimit({
     windowMs: 10 * 1000 * 60, // 10 minute window
